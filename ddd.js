19645d4f9467d0c62d261ffcdb797c49b658edf1
Maria Pacana <maria.pacana@gmail.com>
2015-09-02 07:13:34 -0700

    Allow audience to vote and display the results.

--- ddd.js
+++ ddd.js
@@ -3,11 +3,16 @@ if (Meteor.isClient) {
     currentRound: function() {
       var currentRound = Rounds.findOne();
       return currentRound;
+    },
+  });
+  Template.winner.helpers({
+    winner: function() {
+      var currentRound = Rounds.findOne();
+      return currentRound && currentRound.winner;
     }
   });
   Template.options.helpers({
     options: function() {
-      debugger;
       return Options.find({round: this.current});
     },
     optionVotes: function() {
@@ -53,16 +58,33 @@ if (Meteor.isServer) {
 
   Meteor.setInterval(function () {
     var max_rounds = 3;
+    var votes;
+    var groupedVotes;
+    var countedVotes;
+    var sortedVotes;
+    var winnerId;
+    var winner;
     var currentRound = Rounds.findOne();
 
-    if (currentRound && currentRound.current < max_rounds) {
+    if (currentRound && currentRound.current <= max_rounds) {
       // Show the result for the current round, by updating it with
       // the correct option
+      votes = Votes.find({round: currentRound.current}).fetch();
+      groupedVotes = _.groupBy(votes, function(vote){return vote.optionId});
+      countedVotes = _.map(groupedVotes, function(votes, optionId){
+                            return {length: votes.length, id: optionId};
+                      });
+      sortedVotes = _.sortBy(countedVotes, function(v){return v.length;});
+      winnerId = sortedVotes[sortedVotes.length - 1].id;
+      winner = Options.findOne(winnerId).text;
+
+      Rounds.update(currentRound._id, {$set: {winner: winner}});
 
       // Move to the next round
       Rounds.update({_id: currentRound._id}, {$inc: {current: 1}});
     } else {
       Rounds.remove({});
+      Votes.remove({});
     }
-  }, 120000);
+  }, 10000);
 }
