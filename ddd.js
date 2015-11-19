7fbca22391cb696ef4f2dcf0fbd0445382424d7b
Maria Pacana <maria.pacana@gmail.com>
2015-11-19 09:54:20 -0800

    Added reset button and moved data manip to server methods

--- ddd.js
+++ ddd.js
@@ -1,68 +1,3 @@
-function clearData() {
-  Options.remove({});
-  Ratings.remove({});
-  Rounds.remove({});
-  Votes.remove({});
-}
-
-function seedData(roundCount) {
-  var options = [{round: 1, text: "juggling"},
-                 {round: 1, text: "singing"},
-                 {round: 1, text: "levitating"},
-                 {round: 2, text: "dancing"},
-                 {round: 2, text: "finding the meaning of life"},
-                 {round: 2, text: "getting prime factors"},
-                 {round: 3, text: "swerving"},
-                 {round: 3, text: "squiggling"},
-                 {round: 3, text: "bouncing"},
-                 {round: 4, text: "calculating"},
-                 {round: 4, text: "relating"},
-                 {round: 4, text: "prognosticating"}];
-
-  if (Rounds.find().count() === 0) {
-    Rounds.insert({roundCount: roundCount, current: true});
-  }
-  if (Options.find().count() === 0) {
-    _.each(options, function(doc) {
-      Options.insert(doc);
-    });
-  }
-}
-
-function runGame(roundCount) {
-  Meteor.setInterval(function () {
-    var max_rounds = 4;
-    var votes;
-    var groupedVotes;
-    var countedVotes;
-    var sortedVotes;
-    var winnerId;
-    var winner;
-    var currentRound = Rounds.findOne({current: true});
-    var currentRoundCount = currentRound.roundCount;
-    var nextRoundCount = currentRoundCount+1;
-
-    if (currentRound && currentRound.roundCount <= max_rounds) {
-      // Show the result for the current round, by updating it with
-      // the correct option
-      votes = Votes.find({round: currentRound.roundCount}).fetch();
-      groupedVotes = _.groupBy(votes, function(vote){return vote.optionId});
-      countedVotes = _.map(groupedVotes, function(votes, optionId){
-                            return {length: votes.length, id: optionId};
-                      });
-      sortedVotes = _.sortBy(countedVotes, function(v){return v.length;});
-      winnerId = sortedVotes[sortedVotes.length - 1].id;
-      winner = Options.findOne(winnerId).text;
-
-      Rounds.update(currentRound._id, {$set: {winner: winner}});
-
-      // Move to the next round
-      Rounds.update({_id: currentRound._id}, {$set: {current: false}});
-      Rounds.insert({roundCount: nextRoundCount, current: true});
-    }
-  }, 10000);
-}
-
 if (Meteor.isClient) {
   Template.ddd.helpers({
     currentRound: function() {
@@ -148,7 +83,7 @@ if (Meteor.isClient) {
     }
   });
   Template.options.events({
-    'click .option': function(){
+    'click .option': function() {
         var userId = Meteor.userId();
         var count = Votes.find({'optionId': this._id,
                                 'userId': userId,
@@ -159,12 +94,86 @@ if (Meteor.isClient) {
                         'round': this.round});
     }
   });
+  Template.restart.events({
+    'click #restart': function() {
+      console.log("restarted");
+      var roundCount = 1;
+      Meteor.call('clearData');
+      Meteor.call('seedData', roundCount);
+      Meteor.call('runGame',roundCount);
+    }
+  })
 }
 
 if (Meteor.isServer) {
+  Meteor.methods({
+    'clearData': function() {
+      Options.remove({});
+      Ratings.remove({});
+      Rounds.remove({});
+      Votes.remove({});
+    },
+    'seedData': function(roundCount) {
+        var options = [{round: 1, text: "juggling"},
+                       {round: 1, text: "singing"},
+                       {round: 1, text: "levitating"},
+                       {round: 2, text: "dancing"},
+                       {round: 2, text: "finding the meaning of life"},
+                       {round: 2, text: "getting prime factors"},
+                       {round: 3, text: "swerving"},
+                       {round: 3, text: "squiggling"},
+                       {round: 3, text: "bouncing"},
+                       {round: 4, text: "calculating"},
+                       {round: 4, text: "relating"},
+                       {round: 4, text: "prognosticating"}];
+
+        if (Rounds.find().count() === 0) {
+          Rounds.insert({roundCount: roundCount, current: true});
+        }
+        if (Options.find().count() === 0) {
+          _.each(options, function(doc) {
+            Options.insert(doc);
+          });
+        }
+    },
+    'runGame': function(roundCount) {
+      Meteor.setInterval(function () {
+        var max_rounds = 4;
+        var votes;
+        var groupedVotes;
+        var countedVotes;
+        var sortedVotes;
+        var winnerId;
+        var winner;
+        var currentRound = Rounds.findOne({current: true});
+        var currentRoundCount = currentRound.roundCount;
+        var nextRoundCount = currentRoundCount+1;
+
+        if (currentRound && currentRound.roundCount <= max_rounds) {
+          // Show the result for the current round, by updating it with
+          // the correct option
+          votes = Votes.find({round: currentRound.roundCount}).fetch();
+          groupedVotes = _.groupBy(votes, function(vote){return vote.optionId});
+          countedVotes = _.map(groupedVotes, function(votes, optionId){
+                                return {length: votes.length, id: optionId};
+                          });
+          sortedVotes = _.sortBy(countedVotes, function(v){return v.length;});
+          winnerId = sortedVotes[sortedVotes.length - 1].id;
+          winner = Options.findOne(winnerId).text;
+
+          Rounds.update(currentRound._id, {$set: {winner: winner}});
+
+          // Move to the next round
+          Rounds.update({_id: currentRound._id}, {$set: {current: false}});
+          Rounds.insert({roundCount: nextRoundCount, current: true});
+        }
+      }, 10000);
+    }
+  });
+
   Meteor.startup(function () {
     var roundCount = 1;
-    seedData(roundCount);
-    runGame(roundCount);
+    Meteor.call('seedData', roundCount);
+    Meteor.call('runGame',roundCount);
   });
 }
