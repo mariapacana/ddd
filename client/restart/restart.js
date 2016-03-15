dcf0f25a8115dd19d5e754569501bbc692aa8aa3
Maria Pacana <maria.pacana@gmail.com>
2016-03-15 07:36:54 -0700

    Move all partial templates into own files and make admin template

--- client/restart/restart.js
+++ client/restart/restart.js
@@ -1,9 +1,7 @@
 Template.restart.events({
   'click #restart': function() {
-    console.log("restarted");
     var roundCount = 1;
     Meteor.call('clearData');
     Meteor.call('seedData', roundCount);
-    Meteor.call('runGame',roundCount);
   }
 });
