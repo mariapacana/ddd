ac10ae800b225531a6f3c9ed099ced0d9a544d0c
Maria Pacana <maria.pacana@gmail.com>
2016-03-22 21:42:17 -0700

    Make projector page

---
+++ client/projector/projector.js
@@ -0,0 +1,17 @@
+Template.projector.helpers({
+  totalScore: function() {
+    var ratings;
+    var ratingValues;
+    var ratingSum;
+    var average = "Not Ready";
+
+    ratings = Ratings.find().fetch();
+    ratingValues = _.pluck(ratings, 'value');
+    if (ratingValues.length !== 0) {
+      ratingSum = ratingValues.reduce(function(a, b) { return a + b; });
+      average = ratingSum / ratingValues.length;
+    }
+
+    return average;
+  }
+});
