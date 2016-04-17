4b2ef5e5285770b0a63988a77fba7786d4da423c
Maria Pacana <maria.pacana@gmail.com>
2016-04-17 07:56:48 -0700

    Total score is only Ramona's for now

--- client/projector/projector.js
+++ client/projector/projector.js
@@ -1,6 +1,6 @@
 Template.projector.helpers({
   totalRating: function() {
-    var totalRating = averageRating(this, true);
+    var totalRating = averageRating(this, "ramona", true);
     return (totalRating === "not ready")
       ? "Not Ready"
       : totalRating;
