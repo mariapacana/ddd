363d7593e7d9188970a1e94368aeb35225adaf2f
Maria Pacana <maria.pacana@gmail.com>
2016-04-29 06:56:20 -0700

    Show confirmation of option selection

--- client/options/options.js
+++ client/options/options.js
@@ -1,21 +1,28 @@
+Template.options.onRendered(function() {
+  Session.set('currentOption', null);
+})
+
 Template.options.helpers({
   options: function() {
     return Options.find({round: this.roundCount});
   },
-  optionVotes: function() {
-    return Votes.find({optionId: this._id, round: this.round}).count();
+  selectedOption: function() {
+    return Session.get('currentOption');
   }
 });
 
 Template.options.events({
   'click .option': function() {
-      var userId = Meteor.userId();
-      var count = Votes.find({'userId': userId,
-                              'round': this.round}).count();
-      if (count === 0)
-        Votes.insert({'userId': userId,
-                      'optionId': this._id,
-                      'round': this.round});
+    var userId = Meteor.userId();
+    var count = Votes.find({'userId': userId,
+                            'round': this.round}).count();
+    if (count === 0) {
+      Session.set('currentOption', this.text);
+      Votes.insert({'userId': userId,
+                    'optionId': this._id,
+                    'round': this.round});
+      Options.update({_id: this._id}, {$inc: {votes: 1}});
+    }
   }
 });
 
